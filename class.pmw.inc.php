ddee915f1b3d10e500af4b360b69db97b0b536f6
Abel Cheung <abelcheung@gmail.com>
2008-09-18 19:15:33 +0000

    Cosmetic changes: indentation and add some comment

--- class.pmw.inc.php
+++ class.pmw.inc.php
@@ -30,79 +30,82 @@
 class pmwRender extends ScoreRender
 {
 
-	/**
-	 * Outputs complete music input file for rendering.
-	 *
-	 * Most usually user supplied content does not contain correct
-	 * rendering options like page margin, staff width etc, and
-	 * each notation has its own requirements. This method adds
-	 * such necessary content to original content for processing.
-	 *
-	 * @return string The full music content to be rendered
-	 */
-	public function get_music_fragment ()
-	{
-		// If page size is changed here, it must also be changed
-		// under conversion_step2().
-		$header = <<<EOD
+/**
+ * Outputs complete music input file for rendering.
+ *
+ * Most usually user supplied content does not contain correct
+ * rendering options like page margin, staff width etc, and
+ * each notation has its own requirements. This method adds
+ * such necessary content to original content for processing.
+ *
+ * @return string The full music content to be rendered
+ */
+public function get_music_fragment ()
+{
+	// If page size is changed here, it must also be changed
+	// under conversion_step2().
+	$header = <<<EOD
 Sheetsize A3
 Linelength {$this->img_max_width}
 Magnification 1.5
 
 EOD;
-		// PMW doesn't like \n\r
-		return str_replace ("\r", '', $header . $this->_input);
-	}
-
-	/**
-	 * Execute the real command for first time rendering
-	 *
-	 * The command reads input content (after prepending and appending
-	 * necessary stuff to user supplied content), and converts it to
-	 * a PostScript file.
-	 *
-	 * @uses ScoreRender::_exec
-	 * @param string $input_file File name of raw input file containing music content
-	 * @param string $intermediate_image File name of rendered PostScript file
-	 * @return boolean Whether rendering is successful or not
-	 */
-	protected function conversion_step1 ($input_file, $intermediate_image)
-	{
-		$cmd = sprintf ('"%s" -includefont -o "%s" "%s"',
-		                $this->mainprog,
-		                $intermediate_image, $input_file);
-		$retval = $this->_exec($cmd);
-
-		return ($result['return_val'] == 0);
-	}
-
-	/**
-	 * @param string $intermediate_image The rendered PostScript file name
-	 * @param string $final_image The final PNG image file name
-	 * @return boolean Whether conversion from PostScript to PNG is successful
-	 * @access protected
-	 */
-	protected function conversion_step2 ($intermediate_image, $final_image)
-	{
-		// ImageMagick mistakenly identify all PostScript produced by PMW as
-		// having Letter (8.5"x11") size! Braindead. Without -page option it
-		// just displays incomprehensible error.
-		return parent::conversion_step2 ($intermediate_image, $final_image, TRUE,
-			'-page a3');
-	}
-
-	/**
-	 * Check if given program is Mup, and whether it is usable.
-	 *
-	 * @param string $prog The program to be checked.
-	 * @return boolean Return true if the given program is Mup AND it is executable.
-	 */
-	public function is_notation_usable ($args = '')
-	{
-		wp_parse_str ($args, $r);
-		extract ($r, EXTR_SKIP);
-		return parent::is_prog_usable ('PMW version', $prog, '-V');
-	}
+	// PMW doesn't like \r\n
+	return str_replace ("\r", '', $header . $this->_input);
 }
+
+/**
+ * Execute the real command for first time rendering
+ *
+ * The command reads input content (after prepending and appending
+ * necessary stuff to user supplied content), and converts it to
+ * a PostScript file.
+ *
+ * @uses ScoreRender::_exec
+ * @param string $input_file File name of raw input file containing music content
+ * @param string $intermediate_image File name of rendered PostScript file
+ * @return boolean Whether rendering is successful or not
+ */
+protected function conversion_step1 ($input_file, $intermediate_image)
+{
+	$cmd = sprintf ('"%s" -includefont -o "%s" "%s"',
+			$this->mainprog,
+			$intermediate_image, $input_file);
+	$retval = $this->_exec($cmd);
+
+	return ($result['return_val'] == 0);
+}
+
+/**
+ * @param string $intermediate_image The rendered PostScript file name
+ * @param string $final_image The final PNG image file name
+ * @return boolean Whether conversion from PostScript to PNG is successful
+ * @access protected
+ */
+protected function conversion_step2 ($intermediate_image, $final_image)
+{
+	// ImageMagick mistakenly identify all PostScript produced by PMW as
+	// having Letter (8.5"x11") size! Braindead. Without -page option it
+	// just displays incomprehensible error. Perhaps PMW is to be blamed
+	// though, since there is no BoundingBox nor page dimension specified
+	// in PostScript produced by PMW.
+	return parent::conversion_step2 ($intermediate_image, $final_image, TRUE,
+		'-page a3');
+}
+
+/**
+ * Check if given program is Mup, and whether it is usable.
+ *
+ * @param string $prog The program to be checked.
+ * @return boolean Return true if the given program is Mup AND it is executable.
+ */
+public function is_notation_usable ($args = '')
+{
+	wp_parse_str ($args, $r);
+	extract ($r, EXTR_SKIP);
+	return parent::is_prog_usable ('PMW version', $prog, '-V');
+}
+
+} // end of class
 
 ?>
