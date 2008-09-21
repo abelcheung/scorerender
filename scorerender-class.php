3387f55548c08372827f1b6681b3205b99a460b1
Abel Cheung <abelcheung@gmail.com>
2008-09-21 14:08:12 +0000

    strip copyright text, repetitive and cumbersome

--- scorerender-class.php
+++ scorerender-class.php
@@ -1,26 +1,5 @@
 <?php
 /*
- ScoreRender - Renders inline music score fragments in WordPress
- Copyright (C) 2006 Chris Lamb <chris at chris-lamb dot co dot uk>
- Copyright (C) 2007, 08 Abel Cheung <abelcheung at gmail dot com>
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
-
-/*
  * Methods implementable by subclasses:
  *
  * Mandatory:
