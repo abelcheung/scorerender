c97cd794fd848f12d02267d0ed9047e7921be796
Abel Cheung <abelcheung@gmail.com>
2009-04-30 03:40:08 +0000

    Use Zero Clipboard flash for cut and paste

--- showcode.php
+++ showcode.php
@@ -1,7 +1,3 @@
-<!DOCTYPE
- html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
- "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-
 <?php
 	// plugin file should be either 2 or 3 levels from WP top dir
 	if (file_exists ('../../../wp-config.php'))
@@ -16,7 +12,10 @@
 		$code = stripslashes ($code);
 
 	define ('TEXTDOMAIN', 'scorerender');
-?>
+
+?><!DOCTYPE
+ html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -25,69 +24,11 @@
 <link rel="stylesheet" type="text/css" href="showcode.css" />
 </head>
 
-<script language="javascript" type="text/javascript">
-<!--
-window.focus();
-
-// http://www.krikkit.net/howto_javascript_copy_clipboard.html
-// http://www.cnblogs.com/zhenyulu/archive/2007/02/08/644362.html
-//
-function CopyClipboard()
-{
-	if (window.clipboardData) // IE
-	{
-		var text="<?php echo addcslashes ($code, "\n\r\"\\"); ?>";
-		window.clipboardData.clearData();
-		window.clipboardData.setData("Text", text);
-		document.getElementById('sr_message').innerHTML = '<?php _e('Successfully copied to clipboard.', TEXTDOMAIN) ?>';
-	}
-	else if (window.netscape && navigator.userAgent.indexOf("Opera") == -1) // Netscape or Mozilla family
-	{
-		try
-		{
-			netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-		}
-		catch (e)
-		{
-			document.getElementById('copybutton').disabled = true;
-			document.getElementById('sr_message').innerHTML = '<?php _e('Automatic copy and paste is not enabled for this browser. Please copy and paste the code below manually.<br />For Gecko based browsers (e.g. Mozilla, Firefox, Flock), there is an alternative method: type &#8220;about:config&#8221; in browser address bar, find the preference &#8220;<code>signed.applets.codebase_principal_support</code>&#8221;, change its value to &#8220;true&#8221; and reload this page.', TEXTDOMAIN) ?>';
-			return false;
-		}
-
-		var text = "<?php echo addcslashes (html_entity_decode (rawurldecode ($code)), "\n\r\"\\"); ?>";
-		var clip = Components.classes['@mozilla.org/widget/clipboard;1'].createInstance(Components.interfaces.nsIClipboard); 
-		if (!clip) 
-			return; 
-		var trans = Components.classes['@mozilla.org/widget/transferable;1'].createInstance(Components.interfaces.nsITransferable); 
-		if (!trans) 
-			return; 
-		trans.addDataFlavor('text/unicode'); 
-		var str = new Object(); 
-		var len = new Object(); 
-		var str = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString); 
-		var copytext = text; 
-		str.data = copytext; 
-		trans.setTransferData("text/unicode",str,copytext.length*3); 
-		var clipid = Components.interfaces.nsIClipboard; 
-		if (!clip) 
-			return false; 
-		clip.setData(trans,null,clipid.kGlobalClipboard);
-
-		document.getElementById('sr_message').innerHTML = '<?php _e('Successfully copied to clipboard.', TEXTDOMAIN) ?>';
-	}
-	else
-	{
-		document.getElementById('copybutton').disabled = true;
-		document.getElementById('sr_message').innerHTML = '<?php _e('Cut and paste not supported yet on this browser. Please select and copy the code below manually.', TEXTDOMAIN) ?>';
-		return false;
-	}
-
-}
-// -->
-</script>
 <body>
-<div id="sr_message"></div>
-<button id="copybutton" onclick="return CopyClipboard();"><?php _e('Copy to Clipboard', TEXTDOMAIN) ?></button><br />
+
+<script type="text/javascript" src="ZeroClipboard.js"></script>
+
+<button id="copybutton"><?php _e('Copy to Clipboard', TEXTDOMAIN) ?></button><br />
 <h3><?php _e('Code:', TEXTDOMAIN) ?></h3>
 <div id="code">
 <pre>
@@ -99,5 +40,22 @@ function CopyClipboard()
 ?>
 </pre>
 </div>
+
+<script language="JavaScript" type="text/javascript">
+	var clip = new ZeroClipboard.Client();
+	clip.setText( '' );
+	clip.setHandCursor( true );
+
+	clip.addEventListener( 'complete', function( client, text ) {
+		alert( 'Music code is successfully copied to clipboard.' );
+	} );
+
+	clip.addEventListener( 'mousedown', function( client ) {
+		clip.setText( "<?php echo addcslashes (html_entity_decode (rawurldecode ($code)), "\n\r\"\\"); ?>" );
+	} );
+
+	clip.glue( 'copybutton' );
+</script>
+
 </body>
 </html>
