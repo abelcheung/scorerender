430043d909cf8399fda1a9e27ca21e9001325cc0
Abel Cheung <abelcheung@gmail.com>
2010-02-12 14:08:25 +0000

    Update copyright, switch to AGPL v3

--- notation/pmw.php
+++ notation/pmw.php
@@ -2,6 +2,10 @@
 /**
  * Implements rendering of Philip's Music Writer notation in ScoreRender.
  * @package ScoreRender
+ * @version 0.3.50
+ * @author Abel Cheung <abelcheung at gmail dot com>
+ * @copyright Copyright (C) 2008, 2009, 2010 Abel Cheung
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU AGPL v3
 */
 
 /**
--- scorerender-class.php
+++ scorerender-class.php
@@ -5,8 +5,8 @@
  * @version 0.3.50
  * @author Abel Cheung <abelcheung at gmail dot com>
  * @copyright Copyright (C) 2006 Chris Lamb <chris at chris-lamb dot co dot uk>
- * @copyright Copyright (C) 2007-09 Abel Cheung
- * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @copyright Copyright (C) 2007, 2008, 2009, 2010 Abel Cheung
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU AGPL v3
  */
 
 /**
